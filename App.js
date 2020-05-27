a737a4dd1658fab0e1ee1a6b257d185b09d7c38e
sangita avghad <sangita.awaghad@gmail.com>
2020-05-26 22:03:53 -0700

    ADDED:created StackNavigator

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 
+import MealNavigator from './navigation/MealNavigator';
+
 const fetchFonts=()=>{
   return Font.loadAsync({
     'open-sans':require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -15,11 +17,9 @@ export default function App() {
   if(!fontLoaded){
     return <AppLoading startAsync={fetchFonts} onFinish={()=>setFontLoaded(true)}/>
   }
-  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealNavigator/>
   );
 }
 
