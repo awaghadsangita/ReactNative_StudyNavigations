d45cf0cf57241eb53803e1a0b9bda374fc4b4782
sangita avghad <sangita.awaghad@gmail.com>
2020-05-26 22:03:09 -0700

    REFACTORED:corrected function names

--- screens/FavouriteScreen.js
+++ screens/FavouriteScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function FavouriteScreen() {
   return (
     <View style={styles.container}>
       <Text>Favourite Screen</Text>
--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function FilterScreen() {
   return (
     <View style={styles.container}>
       <Text>Filter Screen</Text>
